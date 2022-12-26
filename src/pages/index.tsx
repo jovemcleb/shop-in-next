caa21641f6334044241703b1a9bf88105a7255a5
Caleb Lima <lima.cardoso.caleb@gmail.com>
2022-12-26 15:39:17 -0300

    feat(Products): estrutura e estiliza products

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import { styled } from "../styles";
 import { HomeContainer, Product } from "../styles/pages/home.style";
 import tShirt1 from "../assets/1.png";
 
@@ -7,7 +6,15 @@ export default function Home() {
   return (
     <HomeContainer>
       <Product>
-        <Image src={tShirt1} width={520} height={400} alt="Camisa 1" />
+        <Image src={tShirt1} width={520} height={480} alt="Camisa 1" />
+        <footer>
+          <strong>Camisa Beyond</strong>
+          <span>R$ 03,00</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={tShirt1} width={520} height={480} alt="Camisa 1" />
         <footer>
           <strong>Camisa Beyond</strong>
           <span>R$ 03,00</span>
