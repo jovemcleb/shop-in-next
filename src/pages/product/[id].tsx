a318f4eafa67e7a34850f192ebd95dd261a9124e
Caleb Lima <lima.cardoso.caleb@gmail.com>
2023-02-16 18:54:35 -0300

    :sparkles: feat(productPage): implementa estrutura da page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,29 @@
+import Image from "next/image";
 import { useRouter } from "next/router";
+
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product.style";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h2>{JSON.stringify(query)}</h2>;
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image/> */}</ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta</h1>
+        <span>R$ 50,00</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+          repellendus, architecto reiciendis veritatis modi id, dolore earum
+          iusto, voluptates possimus harum soluta veniam voluptatibus non sint
+          assumenda nisi commodi voluptas.
+        </p>
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
