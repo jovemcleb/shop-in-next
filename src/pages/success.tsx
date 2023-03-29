561e210831d318c72a0923bec763d9168317a122
jovemcleb <lima.cardoso.caleb@gmail.com>
2023-03-29 17:16:40 -0300

    feat(Success): implementa estilo e estrutura da page de Success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <h3>success</h3>;
+  return (
+    <SuccessContainer>
+      <h1>Compra realizada com sucesso!</h1>
+      <ImageContainer></ImageContainer>
+      <p>
+        Uhuul <strong>User</strong>, sua <strong>Product</strong> já está a
+        caminho da sua casa.
+      </p>
+      <Link href="/">Voltar ao Catálogo</Link>
+    </SuccessContainer>
+  );
 }
